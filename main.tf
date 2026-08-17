c267ab01f2543e68e16b16fc46c31edbcc8e717c
root <root@ansible-nginx.wzmayfmx1jgevkqjce5x3m0tsb.rx.internal.cloudapp.net>
2026-08-17 12:25:36 +0000

    digger commit 1

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "digger_demo" {
+  name     = "rg-digger-demo"
+  location = "Central India"
+}
