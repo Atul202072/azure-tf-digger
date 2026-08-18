f1c1ceecbdbd332d860c379bdebb40d1534e58bb
Atul202072 <aksingh.raickwar@gmail.com>
2026-08-18 07:30:33 +0000

    updated maintf for shared-state-tf

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-digger-demo"
+    storage_account_name = "YOUR_STORAGE_ACCOUNT"
+    container_name       = "tfstate"
+    key                  = "digger-demo.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
